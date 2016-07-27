c66cf0af8d6b082921c2496e00b253e97cebff19
david.russell <david.russell@cherwell.com>
2016-07-27 17:28:28 -0600

    Fixed boo boos

--- LNB/LNB/Program.cs
+++ LNB/LNB/Program.cs
@@ -6,41 +6,38 @@ using System.Threading.Tasks;
 
 namespace LNB
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] denom = { 1000, 500, 100, 50, 10, 5, 2, 1 };
-            int amount = 2348;
-            Console.WriteLine("Array line: "+ string.Join(",", lnb(denom, amount).ToArray()));
-            
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] denom = { 500, 100, 50, 10, 5, 2, 1, 1000 };
+			int amount = 2348;
+			List<int> results = lnb(denom, amount);
+			Console.WriteLine("Array line: "+ string.Join(",", results.ToArray()));
+		}
 
-        public static List<int> lnb(int[] arr, int b)
-        {
-            int[] sortedDenom = arr.OrderByDescending(i => (int) i).ToArray();
+		public static List<int> lnb(int[] arr, int b)
+		{
+			int[] sortedDenom = arr.OrderByDescending(i => (int) i).ToArray();
 
-            List<int> result = new List<int>();
+			List<int> result = new List<int>();
 
-            //int[] result;
-            int newAmount = b;
+			//int[] result;
+			int newAmount = b;
 
-            for (int i = 0; i < arr[i]; i++)
-            {
-                int count = newAmount / sortedDenom[i];
-                newAmount = newAmount % sortedDenom[i];
+			for (int i = 0; i < arr.Length; i++)
+			{
+				int count = newAmount / sortedDenom[i];
+				newAmount = newAmount % sortedDenom[i];
 
-                if (count >= 0)
-                {
-                    result.Add(count);
-                    result.Add(arr[i]);
+				if (count >= 0)
+				{
+					result.Add(count);
+					result.Add(sortedDenom[i]);
+				}
+			}
 
-                }
-
-
-            }
-            return result;
-
-        }
-    }
+			return result;
+		}
+	}
 }
