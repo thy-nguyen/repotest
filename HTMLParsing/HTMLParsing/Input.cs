a6b6073c365eb382bdb8e963fe01ac8f98a8f5e1
david.russell <david.russell@cherwell.com>
2016-07-28 10:46:14 -0600

    Initial load of HTMLParsing project

---
+++ HTMLParsing/HTMLParsing/Input.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLParsing
+{
+	public class Input
+	{
+		#region Properties
+
+		public enum DataTypes { Validated, Text, Numeric, Boolean }
+
+		public string InputId { get; private set; }
+		public string ElementId { get; private set; }
+		public DataTypes DataType { get; private set; }
+
+		// Rich client has a multiline flag for Text prompts. Currently, CherwellService does not pass through.
+		public bool MultiLine { get; private set; }
+		public string DisplayText { get; private set; }
+		public bool Required { get; private set; }
+
+		// Ignore mask. Only used by iCherwell to deal with server-side handling of date/time localization.
+		public string Mask { get; private set; }
+
+		// Default value to use for prompt.
+		public string DefaultValue { get; private set; }
+
+		// Ignore for now. iCherwell assumes true and so should Android for now. Indicates whether or
+		// not the user is forced to select from promptValues collection or can enter a alternate value.
+		public bool Enforced { get; private set; }
+
+		// Option list of allowed values for prompt.
+		public List<string> PromptValues { get; set; }
+
+		#endregion
+
+
+
+		#region Constructors
+
+		public Input(string inputId, string elementId, DataTypes dataType, bool multiLine, string displayText,
+			bool required, string mask, string defaultValue, bool enforced, List<string> promptValues)
+		{
+			InputId = inputId;
+			ElementId = elementId;
+			DataType = dataType;
+			MultiLine = multiLine;
+			DisplayText = displayText;
+			Required = required;
+			Mask = mask;
+			DefaultValue = defaultValue;
+			Enforced = enforced;
+			PromptValues = promptValues;
+		}
+
+		#endregion
+
+
+
+		#region Constants
+
+		public static string ImageData = "imagedata";
+		public static string Binary = "binary";
+
+		#endregion
+	}
+}
---
+++ HTMLParsing/HTMLParsing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLParsing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Input> inputs = new List<Input>();
+			inputs.Add(new Input("Input_1", "element_1", Input.DataTypes.Validated, false, "Please select a Cause Code:", true, "", "", true, null));
+			inputs.Add(new Input("Input_2", "element_2", Input.DataTypes.Text, true, "Please enter the close description:", true, "", "", true, null));
+			inputs.Add(new Input("Input_3", "element_3", Input.DataTypes.Validated, false, "Please select a Resolution Code:", true, "", "", true, null));
+
+			string htmlInput = File.ReadAllText(@"Form.html");
+
+
+			PopulatePromptValues(htmlInput, inputs);
+		}
+
+		static void PopulatePromptValues(string htmlInput, List<Input> inputs)
+		{
+			// Validated inputs need their PromptValues collection updated from the passed in html.
+			// Need to find the correct select tag by id and parse out the option values to build up the prompt collection.
+			
+			 
+			
+		} 
+	}
+}
